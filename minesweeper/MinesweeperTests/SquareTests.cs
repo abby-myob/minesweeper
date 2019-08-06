ea9001d64aa93ca7d8c2f4ace6d7f1682930405e
Abby Thompson <abby.thompson@myob.com>
2019-08-07 10:04:27 +1200

    expanding the square test

--- minesweeper/MinesweeperTests/SquareTests.cs
+++ minesweeper/MinesweeperTests/SquareTests.cs
@@ -6,24 +6,37 @@ namespace MinesweeperTests
 {
     public class SquareTests
     {
-        [Fact]
-        public void When_given_neighbours_check_new_state()
+        [Theory]
+        [InlineData(2, '2', '.')]
+        [InlineData(2, '*', '*')]
+        [InlineData(8, '8', '.')]
+        [InlineData(0, '0', '.')]
+        [InlineData(0, '*', '*')]
+        public void When_given_neighbours_check_new_state(int numMines, char expected, char squareState)
         {
-            Square square = new Square('*');
-            List<Square> neighbours = new List<Square>();
-            
-            neighbours.Add(new Square('.'));
-            neighbours.Add(new Square('.'));
-            neighbours.Add(new Square('.'));
-            neighbours.Add(new Square('*'));
-            neighbours.Add(new Square('.'));
-            neighbours.Add(new Square('*'));
-            neighbours.Add(new Square('.'));
-            neighbours.Add(new Square('.'));
+            Square square = new Square(squareState);
+            List<Square> neighbours = SetUpNeighbours(numMines);
 
             square.Update(neighbours);
             
-            Assert.Equal('2', square.AdjacentMines);
+            Assert.Equal(expected, square.AdjacentMines);
+        }
+
+        private List<Square> SetUpNeighbours(int numMines)
+        {
+            List<Square> neighbours = new List<Square>();
+            
+            for (int i = 0; i < numMines; i++)
+            {
+                neighbours.Add(new Square('*'));
+            }
+            
+            for (int i = numMines; i < 8; i++)
+            {
+                neighbours.Add(new Square('.'));
+            }
+
+            return neighbours;
         }
     }
 }
